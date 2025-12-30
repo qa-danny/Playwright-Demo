d91b99b72da5690949919c518265ab4f9e1669be
Daniel Schaich <dlschaich@gmail.com>
2025-12-30 09:50:12 -0800

    test and config updates

--- tests/article.test.ts
+++ tests/article.test.ts
@@ -2,7 +2,7 @@ import { test as setup, tags } from '../fixtures/test-options';
 import { expect } from '@playwright/test';
 
 setup.describe('Article Tests Demo', { tag: tags.SMOKE }, () => {
-  setup('should access Checkbox page function', async ({ articlePage }) => {
+  setup('Should access Checkbox page', async ({ articlePage }) => {
     await setup.step('Navigate to article page', async () => {
       await articlePage.navigateToArticle('checkboxes');
       const title = await articlePage.getArticleTitle();
@@ -20,5 +20,36 @@ setup.describe('Article Tests Demo', { tag: tags.SMOKE }, () => {
       const isFirstCheckedAfterToggle = await articlePage.isCheckboxChecked(0);
       expect(isFirstCheckedAfterToggle).toBeTruthy();
     });
+  });
+  setup('Should access Add/Remove Elements page', async ({ articlePage }) => {
+    await setup.step('Navigate to Add/Remove Elements Page', async () => {
+      await articlePage.navigateToArticle('add_remove_elements/');
+      expect(await articlePage.getArticleTitle()).toBeDefined();
+      expect(await articlePage.getArticleTitle()).toContain('Add/Remove Elements');
+    });
+    await setup.step('Click Add Element Button Three Times', async () => {
+      await articlePage.expectNumDeleteButtonsPresent(0);
+      await articlePage.clickAddElementButton();
+      await articlePage.expectNumDeleteButtonsPresent(1);
+      await articlePage.clickAddElementButton();
+      await articlePage.expectNumDeleteButtonsPresent(2);
+      await articlePage.clickAddElementButton();
+      await articlePage.expectNumDeleteButtonsPresent(3);
+    });
+    await setup.step('Click Delete buttons', async () => {
+      /*
+        Because there are multiple Delete Buttons added with the same locator,
+        We will have to either A) call a separate function to say "click first"
+        or B) add in a parameter for an index-based locator.
+
+        Let's add in a parameter for index based, and then in the function do some magic
+      */
+      await articlePage.clickDeleteButton(2);
+      await articlePage.expectNumDeleteButtonsPresent(2);
+      await articlePage.clickDeleteButton(1);
+      await articlePage.expectNumDeleteButtonsPresent(1);
+      await articlePage.clickDeleteButton();
+      await articlePage.expectNumDeleteButtonsPresent(0);
+    })
   });
 });
