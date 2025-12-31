5afbce983eadc07e97e21847500c569735329faa
Daniel Schaich <dlschaich@gmail.com>
2025-12-30 16:05:22 -0800

    playwright config and env updates (#11)

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,4 +1,11 @@
 import { defineConfig, devices } from '@playwright/test';
+import dotenv from 'dotenv';
+import path from 'path';
+
+dotenv.config({
+  path: path.resolve(__dirname, '.env'),
+  quiet: true
+});
 
 const desktopViewport = { width: 1350, height: 800 };
 const mobileViewport = { width: 430, height: 845 };
@@ -18,7 +25,7 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: [['html'], ['list', { printSteps: true }], ['junit', { outputFile: 'test-results/junit/results.xml' }]],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Can use CLI Override options if provided. */
--- tests/example.test.ts
+++ tests/example.test.ts
@@ -2,14 +2,15 @@ import { test, expect } from '@playwright/test';
 
 test.describe('Example tests', () => {
   test('has title', async ({ page }) => {
-    await page.goto('https://playwright.dev/');
+    // have to add a non-null assertion (the ! at the end) to make sure it is a string
+    await page.goto(process.env.PLAYWRIGHTURL!);
 
     // Expect a title "to contain" a substring.
     await expect(page).toHaveTitle(/Playwright/);
   });
 
   test('get started link', async ({ page }) => {
-    await page.goto('https://playwright.dev/');
+    await page.goto(process.env.PLAYWRIGHTURL!);
 
     // Click the get started link.
     await page.getByRole('link', { name: 'Get started' }).click();
